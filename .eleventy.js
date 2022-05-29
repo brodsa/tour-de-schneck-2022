7e7811be70b462601c253c973b353db5388caabe
brodsa <brodinova@gmail.com>
2022-05-29 11:51:36 +0200

    favicon moved to _site

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,8 @@ module.exports = function(eleventyConfig) {
   
     // Zkopírovat css/ to _site/css/
     eleventyConfig.addPassthroughCopy("css");
+
+    eleventyConfig.addPassthroughCopy("favicon");
   
     return {
       // možné formáty šablon
